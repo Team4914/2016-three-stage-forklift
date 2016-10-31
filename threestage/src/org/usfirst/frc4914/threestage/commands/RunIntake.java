0fa1bc2f1726de27ac3412ef45f0979268d1d4d9
cyamonide <cyamonide@gmail.com>
2016-10-31 10:20:04 -0400

    Intake changes
    Slowed bottom flywheel speed in reverse intake
    Runs top flywheel during intake

--- threestage/src/org/usfirst/frc4914/threestage/commands/RunIntake.java
+++ threestage/src/org/usfirst/frc4914/threestage/commands/RunIntake.java
@@ -31,6 +31,7 @@ public class RunIntake extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.shooter.setIntake(1);
+    	Robot.shooter.setTopFlywheel(0.25);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -41,6 +42,7 @@ public class RunIntake extends Command {
     // Called once after isFinished returns true
     protected void end() {
     	Robot.shooter.setIntake(0);
+    	Robot.shooter.setTopFlywheel(0);
     }
 
     // Called when another command which requires one or more of the same
